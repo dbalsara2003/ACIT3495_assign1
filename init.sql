41756ff98364546589a17734327c7f8f5cf05278
petervanderhook <petervanderhook@gmail.com>
2023-02-06 18:16:16 -0800

    Added volume for nginx conf.

--- init.sql
+++ init.sql
@@ -1,8 +1,4 @@
-USE DATABASE video_streaming
-CREATE TABLE video_library (
-  video_id INT NOT NULL AUTO_INCREMENT,
-  video_title VARCHAR(255) NOT NULL,
-  video_path VARCHAR(255) NOT NULL,
-  PRIMARY KEY (video_id)
-);
+CREATE DATABASE video_streaming;
+USE video_streaming;
+CREATE TABLE video_library (video_id INT NOT NULL AUTO_INCREMENT, video_title VARCHAR(255) NOT NULL, video_path VARCHAR(255) NOT NULL, PRIMARY KEY (video_id));
 
