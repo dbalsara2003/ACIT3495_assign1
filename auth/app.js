f82961cac61f7c7d6150caf5ac5de6d76f4e820a
Tamim <tamimyasuo@gmail.com>
2023-02-06 20:26:03 -0800

    Authentication should work now

--- auth/app.js
+++ auth/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const basicAuth = require("basic-auth");
 
 const app = express();
 const users = {
@@ -9,20 +8,34 @@ const users = {
 };
 
 const authenticate = (req, res, next) => {
-  const user = basicAuth(req);
-  if (!user || !users[user.name] || user.pass !== users[user.name]) {
-    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
-    return res.sendStatus(401);
+  req.body = req.body || {};
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password || users[username] !== password) {
+    return res.status(401).send("Unauthorized");
   }
   next();
 };
 
+app.use(function(req, res, next) {
+    if (req.method === 'POST' || req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });  
+
+app.use(express.json());
 app.use(authenticate);
 
-app.get("/", (req, res) => {
+
+app.post("/", (req, res) => {
   res.send("Welcome to the protected page!");
 });
 
 app.listen(3000, () => {
-  console.log("BasicAuth service listening on port 3000");
+  console.log("Auth service listening on port 3000");
 });
