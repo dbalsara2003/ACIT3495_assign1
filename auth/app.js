6f36bc4f1121d03c761712b87020daed760a82c1
Tamim <tamimyasuo@gmail.com>
2023-02-08 08:16:47 -0800

    Man I just want to sleep...

--- auth/app.js
+++ auth/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bp = require("body-parser");
 
 const app = express();
 const users = {
@@ -7,33 +8,71 @@ const users = {
   "peter": "peter"
 };
 
-const authenticate = (req, res, next) => {
-  req.body = req.body || {};
-  const username = req.body.username;
-  const password = req.body.password;
-  if (!username || !password || users[username] !== password) {
-    return res.status(401).send("Unauthorized");
-  }
-  next();
-};
+// const authenticate = (req, res, next) => {
+//   if (req.body === undefined) {
+//     return res.status(401).send("UNAUTHORIZED!!!");
+//   }
 
-app.use(function(req, res, next) {
-    if (req.method === 'POST' || req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });  
+//   if (req.body.username === undefined || req.body.password === undefined) {
+//     return res.status(401).send("UNAUTHORIZED!!!");
+//   }
+
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   if (users[username] !== password) {
+//     return res.status(401).send("UNAUTHORIZED!!!");
+//   }
+//   else {
+//     res.sendStatus(200);
+//   }
+// };
+
+// app.use(function(req, res, next) {
+//     if (req.method === 'OPTIONS') {
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+//       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//       res.status(201).send();
+//     } else if (req.method === 'POST') {
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+//       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     }
+//     else {
+//       next();
+//     }
+//   });  
 
 app.use(express.json());
-app.use(authenticate);
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json());
+// app.use(authenticate);
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
+app.post("/login", async (req, res) => {
+  const body = await req.body;
 
-app.post("/", (req, res) => {
-  res.send("Welcome to the protected page!");
+  if (body === undefined) {
+    return res.status(401).send("UNAUTHORIZED!!!");
+  }
+
+  if (body.username === undefined || body.password === undefined) {
+    return res.status(401).send("UNAUTHORIZED!!!");
+  }
+
+  const username = body.username;
+  const password = body.password;
+  if (users[username] !== password) {
+    return res.status(401).send("UNAUTHORIZED!!!");
+  }
+  else {
+    return res.status(200).send("Authorized");
+  }
 });
 
 app.listen(3000, () => {
--- upload/app.js
+++ upload/app.js
@@ -19,6 +19,17 @@ con.connect((err) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/upload.html", (err) => {
+    if (err) {
+      console.error("Error displaying upload page:", err);
+    }
+    else {
+      console.log("Displaying upload page successfully!");
+    }
+  });
+});
+
 app.listen(5000, () => {
   console.log("Upload service listening on port 5000");
 });
