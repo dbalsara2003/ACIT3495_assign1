b4345e9eb74818c46a4dd13844e1b07ed3ee00a6
ligmanoots <dbalsara2003@gmail.com>
2023-02-08 12:29:14 -0800

    dont need to connect on the db here

--- upload/app.js
+++ upload/app.js
@@ -3,22 +3,6 @@ const mysql = require("mysql2");
 
 const app = express();
 
-const con = mysql.createConnection({
-    host: "db",
-    user: "root",
-    port: 3306,
-    password: "password",
-    database: "video_streaming"
-  });
-con.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    process.exit(1);
-  } else {
-    console.log('Connected to the database successfully!');
-  }
-});
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/upload.html", (err) => {
     if (err) {
