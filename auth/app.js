5907c7a82a307dfff99cbf75be5d53f763bfb510
Tamim Hemat <tamimyasuo@gmail.com>
2023-04-02 11:22:47 +0000

    Everything is now working

--- auth/app.js
+++ auth/app.js
@@ -1,80 +1,72 @@
-const express = require("express");
-const bp = require("body-parser");
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
-const users = {
-  "tamim": "tamim",
-  "daryush": "daryush",
-  "peter": "peter"
-};
 
-// const authenticate = (req, res, next) => {
-//   if (req.body === undefined) {
-//     return res.status(401).send("UNAUTHORIZED!!!");
-//   }
-
-//   if (req.body.username === undefined || req.body.password === undefined) {
-//     return res.status(401).send("UNAUTHORIZED!!!");
-//   }
-
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   if (users[username] !== password) {
-//     return res.status(401).send("UNAUTHORIZED!!!");
-//   }
-//   else {
-//     res.sendStatus(200);
-//   }
-// };
-
-// app.use(function(req, res, next) {
-//     if (req.method === 'OPTIONS') {
-//       res.header('Access-Control-Allow-Origin', '*');
-//       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//       res.status(201).send();
-//     } else if (req.method === 'POST') {
-//       res.header('Access-Control-Allow-Origin', '*');
-//       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//     }
-//     else {
-//       next();
-//     }
-//   });  
-
-app.use(express.json());
-app.use(bp.urlencoded({ extended: true }));
-app.use(bp.json());
-// app.use(authenticate);
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+// Configure MySQL connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'mysql',
+  user: 'root',
+  password: 'password',
+  database: 'video_streaming'
 });
 
-app.post("/login", async (req, res) => {
-  const body = await req.body;
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-  if (body === undefined) {
-    return res.status(401).send("UNAUTHORIZED!!!");
-  }
+// Login endpoint
+app.post('/auth/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  if (body.username === undefined || body.password === undefined) {
-    return res.status(401).send("UNAUTHORIZED!!!");
-  }
+  // Query the database to check if the username and password are correct
+  pool.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], (error, results, fields) => {
+    if (error) throw error;
 
-  const username = body.username;
-  const password = body.password;
-  if (users[username] !== password) {
-    return res.status(401).send("UNAUTHORIZED!!!");
-  }
-  else {
-    return res.status(200).send("Authorized");
-  }
+    if (results.length > 0) {
+      // Login successful, redirect to the custom URL
+      const uploadPage = 'http://178.128.226.60/upload';
+      request(uploadPage, (error, response, body) => {
+        if (error) throw error;
+        res.redirect(uploadPage);
+      });
+    } else {
+      // Login failed
+      res.redirect('/auth');
+    }
+  });
 });
 
+// Sign up endpoint
+app.post('/auth/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Insert new user into the database
+  pool.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, password], (error, results, fields) => {
+    if (error) throw error;
+
+    // Redirect back to the login page
+    res.redirect('/auth');
+  });
+});
+
+// Render login page
+app.get('/auth', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+// Render sign up page
+app.get('/auth/signup', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+// Start server
 app.listen(3000, () => {
-  console.log("Auth service listening on port 3000");
+  console.log('Server started on port 3000');
 });
+
